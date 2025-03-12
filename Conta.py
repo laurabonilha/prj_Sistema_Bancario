3e5538ac5833bf3d6557ca28fcc91adb252f1518
laurabonilha <bonilha.laura@gmail.com>
2025-03-12 15:40:55 -0300

    Construção principal das classes Cliente, Conta e Pessoa Física

---
+++ Conta.py
@@ -0,0 +1,53 @@
+class Conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = '0001'
+        self._cliente = cliente
+        self._historico = Historico()
+        
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls (numero, cliente)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
+        
+        if excedeu_saldo:
+            print('Operação falhou. Não há saldo suficiente')
+        elif valor > 0:
+            self._saldo -= valor
+            print('Saque realizado com sucesso')
+            return True
+        else:
+            print('Operação falhou. O valor informado é inválido')
+            
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            print('Depósito realizado com sucesso')
+        else:
+            print('Operação falhou. Valor informado é inválido')
+            return False
+        
